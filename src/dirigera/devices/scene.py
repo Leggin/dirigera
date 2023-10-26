101c5f63f8201e4bf2f1df946838ae49193d788a
Fernando Birra <fpb@fct.unl.pt>
2023-10-27 00:25:43 +0100

    Fix for optional timestamp fields in scenes

--- src/dirigera/devices/scene.py
+++ src/dirigera/devices/scene.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 
 from ..hub.abstract_smart_home_hub import AbstractSmartHomeHub
 
@@ -10,9 +10,9 @@ class Scene:
     scene_id: str
     name: str
     icon: str
-    last_completed: str
-    last_triggered: str
-    last_undo: str
+    last_completed: Optional[str]
+    last_triggered: Optional[str]
+    last_undo: Optional[str]
 
     def trigger(self) -> None:
         self.dirigera_client.post(route=f"/scenes/{self.scene_id}/trigger")
@@ -27,7 +27,7 @@ def dict_to_scene(data: Dict[str, Any], dirigera_client: AbstractSmartHomeHub) -
         scene_id=data["id"],
         name=data["info"]["name"],
         icon=data["info"]["icon"],
-        last_completed=data["lastCompleted"],
-        last_triggered=data["lastTriggered"],
-        last_undo=data["lastUndo"]
+        last_completed=data.get("lastCompleted"),
+        last_triggered=data.get("lastTriggered"),
+        last_undo=data.get("lastUndo")
     )

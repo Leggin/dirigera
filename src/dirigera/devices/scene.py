71cc0e0133cba8228fbd8c009117fa89c83bb44d
Fernando Birra <fpb@fct.unl.pt>
2023-10-27 02:06:41 +0100

    Added refresh method to Scene class to sync action timestamps

--- src/dirigera/devices/scene.py
+++ src/dirigera/devices/scene.py
@@ -14,6 +14,16 @@ class Scene:
     last_triggered: Optional[str]
     last_undo: Optional[str]
 
+    def refresh(self) -> None:
+        data = self.dirigera_client.get(route=f"/scenes/{self.scene_id}")
+        self.scene_id = data["id"]
+        self.name = data["info"]["name"]
+        self.icon = data["info"]["icon"]
+        self.last_completed=data.get("lastCompleted")
+        self.last_triggered=data.get("lastTriggered")
+        self.last_undo=data.get("lastUndo")
+        
+
     def trigger(self) -> None:
         self.dirigera_client.post(route=f"/scenes/{self.scene_id}/trigger")
 

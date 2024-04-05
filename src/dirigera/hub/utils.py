ff1ac73f157bba843b59ea2ae43980ad4a89b0b6
Leggin <legginsun@gmail.com>
2024-04-05 17:01:59 +0200

    fiy mypy

--- src/dirigera/hub/utils.py
+++ src/dirigera/hub/utils.py
@@ -3,7 +3,7 @@ import re
 from typing import Dict, List, Union
 
 
-def camelize_dict(data: Union[Dict, List]):
+def camelize_dict(data: Union[Dict, List]) -> Dict:
     camelize = functools.partial(re.sub, r"_([a-z])", lambda m: m.group(1).upper())
 
     if isinstance(data, list):

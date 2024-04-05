0f7effed07a1c1f34f2caabb1cacb4d062601988
Leggin <legginsun@gmail.com>
2024-04-05 16:59:12 +0200

    fix linter issues

--- src/dirigera/hub/utils.py
+++ src/dirigera/hub/utils.py
@@ -3,12 +3,12 @@ import re
 from typing import Dict, List, Union
 
 
-def camelize_dict(d: Union[Dict, List]):
+def camelize_dict(data: Union[Dict, List]):
     camelize = functools.partial(re.sub, r"_([a-z])", lambda m: m.group(1).upper())
 
-    if isinstance(d, list):
-        return [camelize_dict(i) if isinstance(i, (dict, list)) else i for i in d]
+    if isinstance(data, list):
+        return [camelize_dict(i) if isinstance(i, (dict, list)) else i for i in data]
     return {
         camelize(a): camelize_dict(b) if isinstance(b, (dict, list)) else b
-        for a, b in d.items()
+        for a, b in data.items()
     }
